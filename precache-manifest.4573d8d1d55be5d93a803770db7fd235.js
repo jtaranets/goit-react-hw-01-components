cbc72debd6b85a0b25f52b1dd82c8bdccd8adbee
Julia Taranets <jtaranets@gmail.com>
2019-09-30 17:44:12 +0300

    Updates

---
+++ precache-manifest.4573d8d1d55be5d93a803770db7fd235.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39eefe51b69e53866e4a1a66b163c998",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "365337dcf9c7c9bdf72d",
+    "url": "/goit-react-hw-01-components/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ef120f6d799827d2afdf",
+    "url": "/goit-react-hw-01-components/static/css/main.025ca38a.chunk.css"
+  },
+  {
+    "revision": "365337dcf9c7c9bdf72d",
+    "url": "/goit-react-hw-01-components/static/js/2.10b57136.chunk.js"
+  },
+  {
+    "revision": "ef120f6d799827d2afdf",
+    "url": "/goit-react-hw-01-components/static/js/main.6839e66c.chunk.js"
+  },
+  {
+    "revision": "daffbc77bd7db6c6a4c0",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.d56aa54c.js"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-700.1efbd38a.woff2"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-700.874b8e7b.woff"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-regular.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-regular.b8ee546a.woff"
+  }
+]);
